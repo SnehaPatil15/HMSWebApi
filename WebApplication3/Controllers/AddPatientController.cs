a852e9896f040a17b581cdbca82ca6c414353331
SnehaPatil15 <sneha_patil2@persistent.com>
2022-05-19 15:45:27 +0530

    Added Put method

--- WebApplication3/Controllers/AddPatientController.cs
+++ WebApplication3/Controllers/AddPatientController.cs
@@ -20,10 +20,32 @@ namespace WebApplication3.Controllers
         {
             _configuration = configuration;
         }
+        //[HttpGet]
+        //public JsonResult Get(int pid)
+        //{
+        //    string query = @"select * from AddPatient where pid=@pid"; //where pid=1 or where pid=@pid
+        //    DataTable table = new DataTable();
+        //    string sqlDataSourse = _configuration.GetConnectionString("AddPatientAppCon");
+        //    SqlDataReader myReader;
+        //    using (SqlConnection myCon = new SqlConnection(sqlDataSourse))
+        //    {
+        //        myCon.Open();
+        //        using (SqlCommand myCommand = new SqlCommand(query, myCon))
+        //        {
+        //            myCommand.Parameters.AddWithValue("@pid", pid);
+        //            myReader = myCommand.ExecuteReader();
+        //            table.Load(myReader);
+        //            myReader.Close();
+        //            myCon.Close();
+        //        }
+        //    }
+        //    return new JsonResult(table);
+
+        //}
         [HttpGet]
         public JsonResult Get()
         {
-            string query = @"select * from AddPatient where pid=1"; //where pid=1
+            string query = @"select * from AddPatient"; //where pid=1 or where pid=@pid
             DataTable table = new DataTable();
             string sqlDataSourse = _configuration.GetConnectionString("AddPatientAppCon");
             SqlDataReader myReader;
@@ -32,6 +54,7 @@ namespace WebApplication3.Controllers
                 myCon.Open();
                 using (SqlCommand myCommand = new SqlCommand(query, myCon))
                 {
+
                     myReader = myCommand.ExecuteReader();
                     table.Load(myReader);
                     myReader.Close();
@@ -60,7 +83,7 @@ namespace WebApplication3.Controllers
         //            myCommand.Parameters.AddWithValue("@Gender", AP.Gender);
         //            myCommand.Parameters.AddWithValue("@Blood_Group", AP.Blood_Group);
         //            myCommand.Parameters.AddWithValue("@Major_Disease", AP.Major_Disease);
-                   
+
         //            myReader = myCommand.ExecuteReader();
         //            table.Load(myReader);
         //            myReader.Close();
@@ -71,29 +94,36 @@ namespace WebApplication3.Controllers
 
 
         //}
-        //[HttpPut]
-        //public JsonResult Put(AddPatient AP)
-        //{
-        //    string query = @"update AddPatient set Name=@Name where pid=@pid"; //where pid=1
-        //    DataTable table = new DataTable();
-        //    string sqlDataSourse = _configuration.GetConnectionString("AddPatientAppCon");
-        //    SqlDataReader myReader;
-        //    using (SqlConnection myCon = new SqlConnection(sqlDataSourse))
-        //    {
-        //        myCon.Open();
-        //        using (SqlCommand myCommand = new SqlCommand(query, myCon))
-        //        {
-        //            myCommand.Parameters.AddWithValue("@pid", AP.pid);
-        //            myCommand.Parameters.AddWithValue("@Name", AP.Name);
-        //            myReader = myCommand.ExecuteReader();
-        //            table.Load(myReader);
-        //            myReader.Close();
-        //            myCon.Close();
-        //        }
-        //    }
-        //    return new JsonResult(table);
+        [HttpPut]
+        public JsonResult Put(AddPatient AP)
+        {
+            string query = @"update AddPatient set Name=@Name,Full_Address=@Full_Address,Contact=@Contact,Age=@Age,Gender=@Gender ,
+                           Blood_Group=@Blood_Group,Major_Disease=@Major_Disease where pid=@pid"; //where pid=1
+            DataTable table = new DataTable();
+            string sqlDataSourse = _configuration.GetConnectionString("AddPatientAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSourse))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myCommand.Parameters.AddWithValue("@pid", AP.pid);
+                    myCommand.Parameters.AddWithValue("@Name", AP.Name);
+                    myCommand.Parameters.AddWithValue("@Full_Address", AP.Full_Address);
+                    myCommand.Parameters.AddWithValue("@Contact", AP.Contact);
+                    myCommand.Parameters.AddWithValue("@Age", AP.Age);
+                    myCommand.Parameters.AddWithValue("@Gender", AP.Gender);
+                    myCommand.Parameters.AddWithValue("@Blood_Group", AP.Blood_Group);
+                    myCommand.Parameters.AddWithValue("@Major_Disease", AP.Major_Disease);
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult(table);
 
-        //}
+        }
         //[HttpDelete]
         //public JsonResult Delete(int pid)
         //{
